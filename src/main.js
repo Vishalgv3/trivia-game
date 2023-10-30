ff987bc5bff1960b7fecb84e3820369d2e644b48
Vishalgv3 <vishalcanada03@gmail.com>
2023-10-30 18:43:44 -0300

    intial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// importing the sass stylesheet for bundling
+import "./../sass/styles.scss";
+
+// JS content goes here
+// ...
+
+function main() {
+    let message = "Hello world!";
+
+    document.getElementById("message2").innerHTML = message;
+    document.getElementById("message1").innerHTML = "My first JS program...";
+    document.getElementById("message3").innerHTML = "HEHEHEHEHEHEHEH.....";
+    console.log("main function did this");
+}
+
+main();
