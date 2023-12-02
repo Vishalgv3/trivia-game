82b8cfcaea19b3121b0b0acc79cd4aa3cacbdc1a
Vishalgv3 <vishalcanada03@gmail.com>
2023-12-02 09:14:48 -0400

    unstaged build directory, made buttons work

--- src/main.js
+++ src/main.js
@@ -5,22 +5,71 @@ const SOURCE = "http://localhost:3000/bundle.json";
 
 let jsonData;
 let qCategories;
+let selectedCategory;
+let pageCategories;
+let btnDown;
+let btnUp;
 
 // -------------------- public methods
 function populateCategories(data) {
-    let categories = data.categories;
+    let jsonCategories = data.categories;
 
     // create the category elements
-    categories.forEach((category, i) => {
+    jsonCategories.forEach((category, i) => {
         let div = document.createElement("div");
         div.classList.add("category" + i);
+        div.classList.add("category");
         div.innerHTML = category.category;
 
         qCategories.appendChild(div);
     });
+
+    // select the first category
+    selectedCategory = document.querySelector(".category0");
+    selectedCategory.classList.add("selected-category");
+
+    // populate the page categories array
+    pageCategories = document.querySelectorAll(".category");
+    console.log(pageCategories);
 }
 
 // -------------------- event handlers
+function onDownClicked(e) {
+    let categoryBelow = selectedCategory.nextElementSibling;
+    
+    if (categoryBelow == null) {
+        // we are at the bottom of the list
+        selectedCategory.classList.remove("selected-category");
+
+        // select the first category
+        selectedCategory = pageCategories[0];
+        selectedCategory.classList.add("selected-category");
+
+    } else {
+        selectedCategory.classList.remove("selected-category");
+        categoryBelow.classList.add("selected-category");
+        selectedCategory = categoryBelow;
+    }
+}
+
+function onUpClicked(e) {
+    let categoryAbove = selectedCategory.previousElementSibling;
+    
+    if (categoryAbove == null) {
+        // we are at the top of the list
+        selectedCategory.classList.remove("selected-category");
+
+        // select the last category
+        selectedCategory = pageCategories[pageCategories.length - 1];
+        selectedCategory.classList.add("selected-category");
+
+    } else {
+        selectedCategory.classList.remove("selected-category");
+        categoryAbove.classList.add("selected-category");
+        selectedCategory = categoryAbove;
+    }
+}
+
 function onResponse(data) {
     console.log(data);
     
@@ -37,6 +86,12 @@ function main() {
 
     // initialization
     qCategories = document.querySelector("#qCategories");
+    btnDown = document.querySelector(".btnDown");
+    btnUp = document.querySelector(".btnUp");
+
+    // wire up the event handlers to the buttons
+    btnDown.addEventListener("click", onDownClicked);
+    btnUp.addEventListener("click", onUpClicked);
 
     // -------------------- fetch data
     fetch(SOURCE)
